d5d11fec2b1caef6016f14f72ebde0be5f08b112
Nathaniel Denny <nathanieldenny@Nathaniels-MacBook-Pro.local>
2024-10-11 17:26:33 +0100

    delete

--- packages/tailwind-firebase-nextjs-referrel-system/scripts/seed.js
+++
@@ -1,5 +0,0 @@
-// import { users } from './placeholder-data.ts';
-const { users } = require("./placeholder-data.ts");
-
-console.log("Testing");
-console.log(users);
--- packages/tailwind-firebase-nextjs-referrel-system/types/invoices.ts
+++
@@ -1,6 +0,0 @@
-export interface Invoice {
-    customer_id: string;
-    amount: number;
-    status: string;
-    date: string;
-}
