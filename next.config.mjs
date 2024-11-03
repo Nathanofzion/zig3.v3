ca260db91b370c3e25407e9afd3e8909e6a482a5
hardworking-toptal-dev <161673729+hardworking-toptal-dev@users.noreply.github.com>
2024-11-03 22:32:30 +0900

    feat: update-next-config-for-passkey

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    'passkey-kit',
+    'passkey-factory-sdk',
+    'passkey-kit-sdk',
+    'sac-sdk',
+  ]
 };
 
 export default nextConfig;
